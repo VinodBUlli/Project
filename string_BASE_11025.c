9e461a0e1629904c70341f4980c7a5e6a71558c8
jerry <vinodibm2@gmail.com>
2018-08-08 09:38:01 +0530

    modified string file

---
+++ string_BASE_11025.c
@@ -0,0 +1,6 @@
+basic string
+adding extra line
+<<<<<<<<< Temporary merge branch 1
+=========
+jerry commit new changes
+>>>>>>>>> Temporary merge branch 2
---
+++ string_REMOTE_11097.c
@@ -0,0 +1,6 @@
+basic string
+adding extra line
+<<<<<<< HEAD
+jerry commit new changes
+=======
+>>>>>>> 6d1bf9d32655ab0f54b12525ebe79571313776c7
