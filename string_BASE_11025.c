c670fe70207bdeba2a68099b7a0c28e8b70ab48c
vinodulli <vinodibm2@gmail.com>
2019-01-26 13:05:28 +0530

    removed not required files

--- string_BASE_11025.c
+++
@@ -1,6 +0,0 @@
-basic string
-adding extra line
-<<<<<<<<< Temporary merge branch 1
-=========
-jerry commit new changes
->>>>>>>>> Temporary merge branch 2
--- string_REMOTE_11097.c
+++
@@ -1,6 +0,0 @@
-basic string
-adding extra line
-<<<<<<< HEAD
-jerry commit new changes
-=======
->>>>>>> 6d1bf9d32655ab0f54b12525ebe79571313776c7
